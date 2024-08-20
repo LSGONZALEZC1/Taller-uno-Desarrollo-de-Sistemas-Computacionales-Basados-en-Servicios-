05cd65753c6d190abb303aee2997697c5ec55d15
LSGONZALEZC1 <120821589+LSGONZALEZC1@users.noreply.github.com>
2024-08-20 18:59:27 -0500

    Add files via upload

---
+++ Laura-Gonzalez/src/main/java/co/com/gonzalez/Laura/Gonzalez/LauraGonzalezApplication.java
@@ -0,0 +1,13 @@
+package co.com.gonzalez.Laura.Gonzalez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauraGonzalezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LauraGonzalezApplication.class, args);
+	}
+
+}
---
+++ Laura-Gonzalez/src/test/java/co/com/gonzalez/Laura/Gonzalez/LauraGonzalezApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.gonzalez.Laura.Gonzalez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauraGonzalezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
